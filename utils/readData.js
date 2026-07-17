14d2c75872411ad634c2d795b7240fb046cd492a
Ayush <ayushjoshi1108@gmail.com>
2026-07-17 17:56:54 +0530

    Add utility module and data.

---
+++ utils/readData.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
+const dataPath = path.join(__dirname, '..', 'data/data.json');
+
+/** Read */
+function readData() {
+  return JSON.parse(fs.readFileSync(dataPath));
+}
---
+++ utils/writeData.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
+const dataPath = path.join(__dirname, '..', 'data/data.json');
+
+/** Write */
+function writeData(data) {
+  fs.writeFileSync(dataPath, JSON.stringify(data, null, 2));
+}
